50c422fdea884995b43a5b1d71a1120d2377f405
zhanpei <zhangzhanpei@gmail.com>
2017-12-07 22:47:49 +0800

    java多线程
    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

---
+++ threads/Global.java
@@ -0,0 +1,5 @@
+package threads;
+
+public class Global {
+    public static int sum = 0; //定义一个所有线程操作的变量
+}
---
+++ threads/Index.java
@@ -0,0 +1,22 @@
+package threads;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Index {
+    public static void main(String[] args) {
+        Global g = new Global();
+        CountDownLatch latch = new CountDownLatch(5); //声明一个计数器，即有5个线程
+        Thread[] threads = new Thread[5];
+        Increment incr = new Increment(g, latch);
+        for (int i = 0; i < 5; i++) {
+            threads[i] = new Thread(incr); //创建线程
+            threads[i].start(); //线程开始执行，会调用 Increment 的 run 方法
+        }
+        try {
+            latch.await(); //等待所有线程执行完毕
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(g.sum); //打印最终结果
+    }
+}
